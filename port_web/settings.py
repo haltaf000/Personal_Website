636d76354a1d8f8712870980e2237680863adf57
Haider <haideraltaf804@gmail.com>
2023-01-09 20:34:58 -0500

    m

--- port_web/settings.py
+++ port_web/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'static/Images'
 
 STATIC_URL = 'static/'
 
--- port_web/urls.py
+++ port_web/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,3 +10,6 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include("members.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
