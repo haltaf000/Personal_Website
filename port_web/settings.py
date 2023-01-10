518b6b511c64a3b6adba4520ad9093fc752796e7
Haider <haideraltaf804@gmail.com>
2023-01-09 21:41:42 -0500

    Add domain name

--- port_web/settings.py
+++ port_web/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-40#hdw@urweu*5
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['personalwebsite-production-1e66.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://personalwebsite-production-1e66.up.railway.app']
+ALLOWED_HOSTS = ['personalwebsite-production-1e66.up.railway.app', 'haideraltaf.com', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://personalwebsite-production-1e66.up.railway.app', 'http://www.haideraltaf.com']
 
 
 # Application definition
